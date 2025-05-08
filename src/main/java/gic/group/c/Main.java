f670e22778409190b8da092246c37d685a820dff
Sonavin <sonavin.rem@gmail.com>
2025-05-08 14:55:41 +0700

    Initial Commit with Java Desktop App

--- src/main/java/gic/group/c/Main.java
+++ src/main/java/gic/group/c/Main.java
@@ -1,7 +1,16 @@
 package gic.group.c;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("My Desktop App");
+            frame.setSize(400, 300);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            JLabel label = new JLabel("Hello from Java Desktop App!", SwingConstants.CENTER);
+            frame.add(label);
+            frame.setVisible(true);
+        });
     }
 }
